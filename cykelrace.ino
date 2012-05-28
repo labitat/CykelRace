16f65e3fab587cac35b6325dc68c3ad5413b5a6e
Rene Mikkelsen <rene.mikkelsen@gmail.com>
2012-05-28 23:57:11 +0200

    Moved to the Labitat-group

--- cykelrace.ino
+++ cykelrace.ino
@@ -7,6 +7,10 @@ int servoStatus0 = 0;            // map'et stilling til servo
 int servoStatus1 = 0;            // map'et stilling til servo
 
 
+enum { READY, RUNNING, DONE } racestate; // READY = vi er klar til at køre - den ene viser står i 'nedtællingsmode'
+                                         // RUNNING = løbet er i gang
+                                         // DONE = den ene løber har vundet, og vi skal ikke længere indlæse data fra inputpins
+                                         
 int buttonPin0 = 9;              // knap til tildeling af bakke - sidder på modstanderens cykel
 // skal kun være aktiv når der er lys i den
 int buttonPin1 = 10;              // knap til tildeling af bakke - sidder på modstanderens cykel
@@ -31,15 +35,15 @@ int extraresActive1 = 0;         // 0 eller 1 alt efter om ekstra modstand er sl
 int resetPin = 4;               // 'hemmelig' knap til reset af løb (medmindre det sker automatisk?)
 int startPin = 5;               // 'hemmelig' knap til start af løb - kan være samme som resetpin (toggle, 3 cycles) / kan også resette og bare starte
 
-int resStart  = 0;              // millis for hvornår timer blev sat igang
+int tidStart  = 0;              // millis for hvornår program blev sat igang
 int hilldDelay = 3000;           // 3 seconds
 int hilltimerStart0 = 0;    // timer for bakke-modstand
 int hilltimerStart1 = 0;    // timer for bakke-modstand
 
 void setup() {
-  int wGoal = 200;                  // definerer målet for løbet (som også er max på skalaen)
-  int wStatus0 = 0;                 // holder styr på hvor langt rytter 0 
-  int wStatus1 = 0;                 // holder styr på hvor langt rytter 1
+  int Goal = 200;                  // definerer målet for løbet (som også er max på skalaen)
+  int Status0 = 0;                 // holder styr på hvor "langt" rytter 0 er nået 
+  int Status1 = 0;                 // holder styr på hvor "langt" rytter 1 er nået
 
   Serial.begin(9600);
 }
@@ -48,17 +52,17 @@ void loop() {
   currentVoltage0 = analogRead(readerPin0);
   currentVoltage1 = analogRead(readerPin1);
 
-  // map currentVoltage efter en ca-skala (0 = 0V, 254 = 12v f.eks.)
-  wStatus0 = (regn effekt ud, efter kendt spænding og kendt modstand og læg sammen med tidligere wStatus);
+  // map currentVoltage0 efter en ca-skala (0 = 0V, 254 = 12v f.eks.)
+  Status0 = (regn effekt ud, efter kendt spænding og kendt modstand og læg sammen med tidligere wStatus);
   servoStatus0 = map(wStatus, 0,0, wGoal, 117); // servoerne kan kun bevæge sig ca 0-120 grader.
   analogWrite(servoPin0, servoStatus0);
-  // map currentVoltage efter en ca-skala (0 = 0V, 254 = 12v f.eks.)
-  wStatus1 = (regn effekt ud, efter kendt spænding og kendt modstand og læg sammen med tidligere wStatus);
+  // map currentVoltage1 efter en ca-skala (0 = 0V, 254 = 12v f.eks.)
+  Status1 = (regn effekt ud, efter kendt spænding og kendt modstand og læg sammen med tidligere wStatus);
   servoStatus1 = map(wStatus1, 0,0, wGoal, 117); // servoerne kan kun bevæge sig ca 0-120 grader.
   analogWrite(servoPin1, servoStatus1);
 
   // vi skal tage stilling til om rytter 0 eller 1 må tildele den anden en "bakke"
-
+  // hvis rytter 0 er 15w foran rytter 1, så kan der tildeles en bakke
 
 
   // er knappen blevet trykket
@@ -71,3 +75,8 @@ void loop() {
   Serial.println(sensorValue);
 }
 
+
+// nulstil viserne mm. og gøre klar til næste løb
+void reset(){
+    
+} 
